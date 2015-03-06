60bd5a0b2505b0e9758a12342a33929bde386998
Caleb Uran <caleb.uran@gmail.com>
2015-03-06 03:41:55 -0600

    I guess I need that comma

--- ModInfo.lua
+++ ModInfo.lua
@@ -1,9 +1,7 @@
 local modinfo = {
 	name = "SKOLM",
-	shortname = "SKOLM",
-	game = "Menu",
 	description = "Some kind of lua menu",
-	version = "alpha"
+	version = "alpha",
 	modtype = "1",
 	depend = {
 		"cursors.sdz",

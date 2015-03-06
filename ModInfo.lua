13f2915a2d3e700f7201dfdc1bb393dc09eddb2a
Caleb Uran <caleb.uran@gmail.com>
2015-03-06 02:50:24 -0600

    Update modinfo.lua

--- ModInfo.lua
+++ ModInfo.lua
@@ -1,13 +1,10 @@
 local modinfo = {
-	name = "StartScreen",
-	shortname = "StartScreen",
-	game = "StartScreen",
-	shortgame = "StartScreen",
-	description = "Game with directory structure + major files",
-	url = "http://www.springrts.com/wiki/SpringABC",
-	
+	name = "SKOLM",
+	shortname = "SKOLM",
+	game = "Menu",
+	description = "Some kind of lua menu",
+	version = "alpha"
 	modtype = "1",
-
 	depend = {
 		"cursors.sdz",
 	}
